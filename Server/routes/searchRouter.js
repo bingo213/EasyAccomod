31b5f641c50b9b5ba9b29d1a86118e9cd14ec201
ChimSe325 <dolong144@gmail.com>
2020-12-22 09:16:24 +0700

    add_search

--- Server/routes/searchRouter.js
+++ Server/routes/searchRouter.js
@@ -19,4 +19,49 @@ searchRouter.route('/getByAddress')
         res.json(posts);
     })
 })
+searchRouter.route('/findRoom')
+.post((req, res, next) =>{
+    var addressDict = new Map();
+    var postArray = [];
+    if(req.body.province){
+        addressDict.set('province' , req.body.province);
+    }
+    if(req.body.district){
+        addressDict.set('district', req.body.district);
+    }
+    if(req.body.village){
+        addressDict.set('village', req.body.village);
+    }
+    if(req.body.price){
+        var arr = req.body.price.split('-');
+        var begin = parseInt(arr[0]);
+        var end = parseInt(arr[1]);
+        postArray.push({price: {$gte : begin}})
+        postArray.push({price: {$lte : end}})
+    }
+    if(req.body.area){
+        var arr = req.body.area.split('-');
+        var begin = parseInt(arr[0]);
+        var end = parseInt(arr[1]);
+        postArray.push({area: {$gte : begin}})
+        postArray.push({area: {$lte : end}})
+    }
+    if (postArray.length ===0 ){
+        Post.find().populate({
+            path: 'address',
+            match: addressDict
+        }).then((posts) =>{
+            res.json(posts);
+        })
+    }
+    else{
+        Post.find({$and: postArray}).populate({
+            path: 'address',
+            match: addressDict
+        }).then((posts) =>{
+            res.json(posts);
+        })
+    }
+    
+})
 module.exports = searchRouter;
